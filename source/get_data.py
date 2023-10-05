6ad0377a9576bb426bab2c21581000a5bfd9eb00
Marlene Elizabeth  Lopez Jimenez <mlopez@biometriaaplicada.com>
2023-10-05 14:11:57 -0600

    ejercicio de integración completo, listo para diseño de experimento

--- source/get_data.py
+++ source/get_data.py
@@ -2,21 +2,30 @@ import os
 import sys
 import argparse
 from typing import Text
+import subprocess
 import dvc.api
 import yaml
 
 # this line was included for git executation errors
 os.environ["GIT_PYTHON_REFRESH"] = "quiet"
 
-def get_data(config_path: Text) -> None:
-    params = yaml.safe_load(open(config_path))["data_acquisition"]
-    #params = yaml.safe_load(open("params.yaml"))["data_acquisition"]
 
-    dvc.api.read(
+def get_data(config_path: Text) -> None:
+
+    params = yaml.safe_load(open(config_path))["data_acquisition"]
+    print(params)
+    
+    if not os.path.isdir("data.zip"):
+        subprocess.run(f"dvc get {params['repo']} {params['path']}")
+        print('Hecho !')
+    else:
+        print('the folder alrready exist')
+
+    '''dvc.api.read(
         params['path'],
         repo = params['repo'],
         mode='r'
-        )
+        )'''
     
 if __name__ == '__main__':
     args_parser = argparse.ArgumentParser()
